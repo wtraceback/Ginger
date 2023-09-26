359bc7c75ff6f5100b0b4a9230ff55a7cde7aa6d
whxcer <whxcer@outlook.com>
2023-09-26 23:33:06 +0800

    使用蓝图来创建分割模块

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/")
-    def index():
-        return 'Hello World'
+    register_blueprints(app)
 
     return app
+
+
+def register_blueprints(app):
+    from app.api.v1.book import book
+    app.register_blueprint(book)
+
+    from app.api.v1.user import user
+    app.register_blueprint(user)
