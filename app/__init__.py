0e6205388675cb72d64e7ec744d2657c99d689d3
whxcer <whxcer@outlook.com>
2024-01-25 23:33:56 +0800

    引入 falsk-sqlalchemy，注册命令行来创建数据库表

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import os
+import click
 
 from app.config import config
 
@@ -12,9 +13,11 @@ def create_app(config_name=None):
     app.config.from_object(config[config_name])
 
     register_blueprints(app)
+    register_plugin(app)
+    register_commands(app)
 
-    for key, value in app.config.items():
-        print(f'{key}={value}')
+    # for key, value in app.config.items():
+    #     print(f'{key}={value}')
 
     return app
 
@@ -22,3 +25,29 @@ def create_app(config_name=None):
 def register_blueprints(app):
     from app.api.v1 import create_blueprint_v1
     app.register_blueprint(create_blueprint_v1(), url_prefix="/v1")
+
+
+def register_plugin(app):
+    from app.models import db
+    db.init_app(app)
+
+
+def register_commands(app):
+    from app.models import db
+
+    @app.cli.command()
+    @click.option('--drop', is_flag=True)       # is_flag=True 表明参数值可以省略
+    def initdb(drop):
+        """
+            生成数据库表
+                如果是sqlite，则 db.create_all() 函数会创建数据库文件以及数据库表
+                如果是mysql，需要提前手动创建数据库，db.create_all() 函数只能创建数据库表
+            使用
+                flask initdb
+                或
+                flask initdb --drop
+        """
+        if drop:
+            db.drop_all()
+        db.create_all()
+        click.echo("数据库表创建成功")
